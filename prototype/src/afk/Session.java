5e4801d404e6aa9475f73be539ee972a5e625584
Ryan Kerr <15rhk@queensu.ca>
2019-10-18 19:59:08 -0400

    Created session loop and handling

--- prototype/src/afk/Session.java
+++ prototype/src/afk/Session.java
@@ -1,6 +1,9 @@
 package afk;
 
 import afk.transactions.Transaction;
+import afk.transactions.TransactionType;
+import afk.transactions.script.TransactionCancelledException;
+import afk.transactions.script.TransactionScript;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -8,17 +11,46 @@ import java.util.List;
 import java.util.Map;
 
 public class Session {
-	boolean finished;
-	String[] summary;
+
+    private SessionType sessionType;
 
     // This is built from the valid account list
-    Map<String, Account> accounts = new HashMap<>();
-    List<Transaction> transactions = new ArrayList<>();
+    private Map<String, Account> accounts = new HashMap<>();
 
-    public Session(Console c, SessionType sessionType, List<Account> validAccounts) {
+    public Session(SessionType sessionType, List<Account> validAccounts) {
+        this.sessionType = sessionType;
+
+        for(Account account : validAccounts)
+            accounts.put(account.getNumber(), account);
     }
 
-    public List<Transaction> logout() {
+    public List<Transaction> run(Console console) {
+        List<Transaction> transactions = new ArrayList<>();
+
+        Transaction transaction;
+        TransactionType command;
+        TransactionScript<?> script;
+
+        do {
+            command = TransactionType.stringToEnum(console.readString());
+
+            if(command == null) {
+                System.out.println("Error: Command not found");
+                continue;
+            }
+
+            try {
+                script = command.getScript();
+            }
+            catch(TransactionCancelledException cancel) {
+                System.out.println("Transaction cancelled");
+                continue;
+            }
+
+            transactions.add(script.execute(console, sessionType, accounts));
+
+        } while(command != TransactionType.END_OF_SESSION);
+
         return transactions;
     }
 }
