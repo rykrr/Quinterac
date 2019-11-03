e129c65e3148ad2f22ff9743f47aa4f6bbeaba7f
Ryan Kerr <15rhk@queensu.ca>
2019-11-03 18:49:12 -0500

    fixed

--- prototype/tests/test.sh
+++ prototype/tests/test.sh
@@ -6,12 +6,19 @@ message=("OK" "FAIL")
 
 fail() {
 	echo "[ FAIL ]"
-	echo "Consult log.txt for more details"
-	[[ $1 -eq 0 ]] && rm -f ../../output_transactions.txt
+	if [[ $1 -eq 0 ]]; then
+		echo "Consult log.txt for more details" 
+		rm -f ../../output_transactions.txt
+	else
+		echo "Transaction outputs differ"
+	fi
 	exit 1
 }
 
 run_case() {
+	rm -f ../../output_transactions.txt
+	rm -f ../../log.txt
+	
 	printf 'Test %-3s %-3s ' $1 $2
 	
 	expect $dir/expect.exp console_output.txt console_input.txt &>/dev/null
@@ -22,11 +29,12 @@ run_case() {
 		diff expected_transactions.txt ../../output_transactions.txt &>/dev/null
 		
 		if [[ $? -ne 0 ]]; then
-			echo "Transaction outputs differ"
 			fail 1
 		fi
 	fi
 	
+	rm -f ../../output_transactions.txt
+	rm -f ../../log.txt
 	echo "[  OK  ]"
 }
 
@@ -34,7 +42,6 @@ for r in $(ls -dx1 r* | sort -V); do
 	pushd $r &>/dev/null
 		for t in $(ls -dx1 t* | sort -V); do
 			pushd $t &>/dev/null
-				rm -f ../../output_transactions.txt
 				echo '1000000' > accounts.txt
 				echo '2000000' >> accounts.txt
 				echo '0000000' >> accounts.txt
