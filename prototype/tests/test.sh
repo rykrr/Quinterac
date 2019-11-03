dff5b42f148219d25b64983f9cba31b5f20a6191
Ryan Kerr <15rhk@queensu.ca>
2019-11-03 18:07:34 -0500

    BASH script tests

--- prototype/tests/test.sh
+++ prototype/tests/test.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-message=(" OK " "FAIL")
+dir=`pwd`
+message=("OK" "FAIL")
+
+
+fail() {
+	echo "[ FAIL ]"
+	echo "Consult log.txt for more details"
+	[[ $1 -eq 0 ]] && rm -f ../../output_transactions.txt
+	exit 1
+}
 
 run_case() {
-	expect expect.exp console_input.txt console_output.txt &>/dev/null
+	printf 'Test %-3s %-3s ' $1 $2
 	
-	status=$?
-	printf 'Test %-3s %-3s    [ %-4s ]\n' $1 $2 ${message[$status]}
+	expect $dir/expect.exp console_output.txt console_input.txt &>/dev/null
 	
-	[[ $status -eq 0 ]] || exit 1
+	[[ $? -eq 0 ]] || fail 0
+	
+	if [[ -e expected_transactions.txt ]]; then
+		diff expected_transactions.txt ../../output_transactions.txt &>/dev/null
+		
+		if [[ $? -ne 0 ]]; then
+			echo "Transaction outputs differ"
+			fail 1
+		fi
+	fi
+	
+	echo "[  OK  ]"
 }
 
 for r in $(ls -dx1 r* | sort -V); do
 	pushd $r &>/dev/null
 		for t in $(ls -dx1 t* | sort -V); do
-			run_case `basename $r` `basename $t`
+			pushd $t &>/dev/null
+				rm -f ../../output_transactions.txt
+				echo '1000000' > accounts.txt
+				echo '2000000' >> accounts.txt
+				echo '0000000' >> accounts.txt
+				run_case `basename $r` `basename $t`
+			popd &>/dev/null
 		done
 	popd &>/dev/null
 done
+
+rm -f log.txt
