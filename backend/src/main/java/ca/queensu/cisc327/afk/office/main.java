e0ec3a0ec63e45ada7695a55b014446f76b911ad
BriannaLi <32473795+BriannaLi@users.noreply.github.com>
2019-11-23 15:13:11 -0500

    update main

--- backend/src/main/java/ca/queensu/cisc327/afk/office/main.java
+++ backend/src/main/java/ca/queensu/cisc327/afk/office/main.java
@@ -1,10 +1,54 @@
 package ca.queensu.cisc327.afk.office;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	
+	}
+	public static void runBackend() {
+		
+	}
+	
+	public static ArrayList<String[]> readTransactiontoList(String file) {
+		ArrayList<String[]> list = new ArrayList<String[]>();
+		try {
+		FileInputStream stream = new FileInputStream(file);
+        DataInputStream in = new DataInputStream(stream);
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String line;
+			while ((line = br.readLine()) != null)   {
+				String[] tokens = line.split(" ");
+				String[] string = {tokens[0], tokens[1], tokens[2], tokens[3]};
+				list.add(string);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("No line found in file.");
+		}
+        return list;
+	}
+	
+	public static Transaction transactionReader(String file) {
+		ArrayList<String[]> list = readTransactiontoList(file);
+		Transaction trans = new Transaction();
+		trans
+	}
+	
+	public static void writetoFile(File file) {
+		
+	}
+	
+	public static void backend() {
+		
 	}
 
 }
